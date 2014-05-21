261f5e6aba1db2e3996bbcf0dda90367aa069881
Jun Xiao <julianxiao@gmail.com>
2014-05-21 11:05:05 -0500

    test

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -3,7 +3,7 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 	$routeProvider
 
 		// home page
-		.when('/', {
+		.when('/sa', {
 			templateUrl: 'views/home.html',
 			controller: 'MainController'
 		})
--- public/js/controllers/MapCtrl.js
+++ public/js/controllers/MapCtrl.js
@@ -3,6 +3,34 @@ angular.module('MapCtrl', []).controller('MapController', function($scope) {
 	$scope.tagline = 'Show Map!';	
 
 
+    var locations = [
+        [30.274210, -97.742727],
+        [30.286829, -97.751875],
+        [30.275583, -97.751319],
+        [30.274583, -97.753519],
+    ];
+
+    var dataPoints = [
+    {
+        title : 'Sensor 1',
+        desc : 'This is the best sensor in the world!',
+        lat : 30.274210,
+        long : -97.72727
+    },
+    {
+        title : 'Sensor 2',
+        desc : 'This sensor is so ...',
+        lat : 30.274583,
+        long : -97.752675
+    },
+    {
+        title : 'Sensor 3',
+        desc : 'This is the second best sensor in the world!',
+        lat : 30.275583,
+        long : -97.751319
+    }
+    ];
+
 
 
     var mapOptions = {
@@ -11,7 +39,7 @@ angular.module('MapCtrl', []).controller('MapController', function($scope) {
         disableDefaultUI: true
     };
 
-    var styleArray = [{
+    var styleArray2 = [{
         featureType: "all",
         stylers: [{
             saturation: -80
@@ -22,7 +50,7 @@ angular.module('MapCtrl', []).controller('MapController', function($scope) {
         stylers: [{
             hue: "#00ffee"
         }, {
-            saturation: 50
+            saturation: 0
         }]
     }, {
         featureType: "poi.business",
@@ -37,7 +65,39 @@ angular.module('MapCtrl', []).controller('MapController', function($scope) {
         }]
     }];
 
+    var styleArray = [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}]
+
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+    $scope.map.setOptions({
+                styles: styleArray
+            });
+
+
+    $scope.markers = [];
+    
+    var infoWindow = new google.maps.InfoWindow();
+    
+    var createMarker = function (info){
+        
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.title
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+        
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open($scope.map, marker);
+        });
+        
+        $scope.markers.push(marker);
+        
+    }  
+    
+    for (i = 0; i < dataPoints.length; i++){
+        createMarker(dataPoints[i]);
+    }
 
 });
