2711821a987b278727fa7dd1280211c09673bb8f
Jun Xiao <julianxiao@gmail.com>
2014-06-03 10:33:35 -0500

    queuview

---
+++ public/analytics/js/controllers/QueueCtrl.js
@@ -0,0 +1,66 @@
+angular.module('QueueCtrl', []).controller('QueueController', function($scope) {
+
+	$scope.tagline = 'Wait in line!';
+
+     var selected = [];
+
+	var table  = $('#example').DataTable({
+         "order": [[ 14, "asc" ]]
+
+
+
+    })
+
+    
+
+    $("#example tfoot th").each( function ( i ) {
+        var select = $('<select id="select' + i + '"><option value=""></option></select>')
+            .appendTo( $(this).empty() )
+            .on( 'change', function () {
+                table.column( i )
+                    .search( $(this).val() )
+                    .draw();
+            } );
+ 
+        table.column( i ).data().unique().sort().each( function ( d, j ) {
+            select.append( '<option value="'+d+'">'+d+'</option>' )
+        } );
+    } );  
+
+        $('a.toggle-vis').on( 'click', function (e) {
+        e.preventDefault();
+ 
+        // Get the column API object
+        var column = table.column( $(this).attr('data-column') );
+ 
+        // Toggle the visibility
+        column.visible( ! column.visible() );
+    } );
+
+/*
+    $('#buttonEdit').click( function () {
+        alert( ' row(s) ' + selected + ' selected. ');
+    } );
+
+
+
+    $('#example tbody').on('click', 'tr', function () {
+        var id = this.id;
+        var index = $.inArray(id, selected);
+ 
+        if ( index === -1 ) {
+            selected.push( id );
+        } else {
+            selected.splice( index, 1 );
+        }
+ 
+        $(this).toggleClass('success');
+
+     } ); */
+
+ /*   var temp = $("#select0");
+    temp.remove();
+	temp = $("#select3");
+	temp.remove(); */
+
+});
