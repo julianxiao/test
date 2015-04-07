23d569303e4efc2f829b86b0a8f976fc53ef4de7
Jun Xiao <julianxiao@gmail.com>
2015-04-07 11:28:40 -0500

    famis

---
+++ public/famis/lib/check.js
@@ -0,0 +1,35 @@
+
+
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}          
+
+
+
+angular.module('demoApp', ['geolocation'])
+  .controller('DemoController', function($scope, geolocation) {
+
+
+
+    $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+    var id = getUrlParameter('id');
+
+    console.log(id);
+    $scope.message = id;
+
+
+
+
+  });
