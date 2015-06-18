ef076ad96711a8beafb2589d026727e28230cad2
Jun Xiao <julianxiao@gmail.com>
2015-06-18 16:59:43 -0500

    grid analytics v0.1

---
+++ gridanalytics/v0.1/index.js
@@ -0,0 +1,49 @@
+var jsonServer = require('json-server');
+
+// Express server
+var server = jsonServer.create();
+
+// Default middlewares (logger, public, cors)
+server.use(jsonServer.defaults);
+
+// Add other Express middlewares if needed (authentication, redirections, ...)
+
+var chance = require('chance');
+var moment = require('moment');
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+server.use(bodyParser.json());
+
+var jsonFile = require('./data/db.json');
+
+for (var i = 0; i < jsonFile.events.length; i++)
+{
+	var d1 = moment(jsonFile.events[i].Date, "M_DD");
+	var date_time = d1.format('2015 MM/DD') + " at " + jsonFile.events[i].Time;
+	jsonFile.events[i].dateTime = date_time;
+}
+
+server.get('/api/schema.json', function(req, res) {
+	var jsonFile = require('./data/schema.json');
+	res.status(200).json(jsonFile);
+
+});
+
+
+
+server.post('/api/config', function(req, res) {
+	configJson = req.body;
+	res.status(200).json(configJson);
+});
+
+
+require('fs').writeFileSync('data/cleaned.json', JSON.stringify(jsonFile, null, 2), 'utf-8');
+
+server.use('/api', jsonServer.router('data/cleaned.json'));
+
+
+server.listen((process.env.PORT || 2000), function() {
+	console.log("grid anaylitics server is up running at port 2000!");
+});
---
+++ gridanalytics/v0.1/public/js/app.js
@@ -0,0 +1,122 @@
+var ulrBase = '/api/events?';
+
+
+var app = angular.module('dataApp', [])
+    .controller('DataController', ['$scope', '$http', function($scope, $http) {
+
+        $scope.items = [];
+        $scope.eventTypeList = [];
+        $scope.sortList = [];
+
+        var offset = 0,
+            eventSelected = '',
+            sortSelected = '';
+
+        $scope.eventTypeList = ['DE', 'NDE'];
+        $scope.sortList = ['Event Magnitude_I', 'Event Magnitude_V', 'Phases Involved', 'Waveform data', 'RMS Data filename'];
+
+        loadItemData(ulrBase);
+
+
+        function loadItemData(url) {
+            $scope.viewLoading = true;
+            $http.get(url).success(function(data, status, headers, config) {
+                $scope.items = data;
+                $scope.viewLoading = false;
+            }).
+            error(function(data, status, headers, config) {
+                console.log(url + ' :reading data error!');
+            });
+        }
+
+        function loadItemDataUrl() {
+            var url = ulrBase;
+            if (eventSelected) {
+                url = url + '&Event%20Type=' + eventSelected;
+            }
+            if (sortSelected) {
+                url = url + '&_sort=' + encodeURI(sortSelected)+ '&_order=DESC';
+            }
+            url = url + '&_limit=20&_start=' + offset;
+            loadItemData(url);
+        }
+
+        $scope.changeType = function(selectValue) {
+            offset = 0;
+            eventSelected = selectValue;
+            loadItemDataUrl();
+        };
+
+        $scope.changeSort = function(selectValue) {
+            offset = 0;
+            sortSelected = selectValue;
+            loadItemDataUrl();
+        };
+
+
+        $scope.loadNext = function() {
+            offset += 20;
+            loadItemDataUrl();
+        };
+
+        $scope.loadPrev = function() {
+            offset -= 20;
+            if (offset < 0) offset = 0;
+            loadItemDataUrl();
+        };
+
+        $scope.loadForward = function() {
+            offset += 100;
+            loadItemDataUrl();
+        };
+
+        $scope.loadBack = function() {
+            offset -= 100;
+            if (offset < 0) offset = 0;
+            loadItemDataUrl();
+        };
+
+    }]);
+
+app.directive('myLoadingSpinner', function() {
+    return {
+        restrict: 'A',
+        replace: true,
+        transclude: true,
+        scope: {
+            loading: '=myLoadingSpinner'
+        },
+        templateUrl: 'loading.html',
+        link: function(scope, element, attrs) {
+            var spinner = new Spinner().spin();
+            var loadingContainer = $(element).find('.my-loading-spinner-container')[0];
+            loadingContainer.appendChild(spinner.el);
+        }
+    };
+});
+
+
+function getQueryParams(qs) {
+    qs = qs.split("+").join(" ");
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while ((tokens = re.exec(qs))) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+function getConfig(data) {
+    var config = null;
+    var query = getQueryParams(document.location.search);
+    config = data.production;
+    if (query.debug==='true') {
+        config = data.development;
+    }
+    if (!config) console.log("error reading configuration data!");
+    return config;
+}
