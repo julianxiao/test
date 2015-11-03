d3bdb1cd82cd05480588d86186b15cefd422d802
Jun Xiao <julianxiao@gmail.com>
2015-11-03 15:19:14 -0600

    bigsv1

--- public/analytics/js/newqueue.js
+++ public/analytics/js/newqueue.js
@@ -1,55 +1,64 @@
 $(document).ready(function() {
 
+
+
     var selected = [];
 
     var variables = [{
-                "data": "Ticket Number"
-            }, {
-                "data": "Call Date"
-            }, {
-                "data": "Ticket Age"
-            }, {
-                "data": "Target Cost Center (TCC)"
-            }, {
-                "data": "Address"
-            }, {
-                "data": "Place"
-            }, {
-                "data": "County"
-            }, {
-                "data": "State"
-            }, {
-                "data": "Contact Name"
-            }, {
-                "data": "Contact Phone"
-            }, {
-                "data": "Excavator Name (Adjusted)"
-            }, {
-                "data": "Ticket Type/Priority/Category"
-            },
+            "data": "Ticket Number"
+        }, {
+            "data": "Call Date"
+        }, {
+            "data": "Ticket Age"
+        }, {
+            "data": "Target Cost Center (TCC)"
+        }, {
+            "data": "Address"
+        }, {
+            "data": "Place"
+        }, {
+            "data": "County"
+        }, {
+            "data": "State"
+        }, {
+            "data": "Contact Name"
+        }, {
+            "data": "Contact Phone"
+        }, {
+            "data": "Excavator Name (Adjusted)"
+        }, {
+            "data": "Ticket Type/Priority/Category"
+        },
 
-            {
-                "data": "Work Done For (Adjusted)"
-            }, {
-                "data": "Excavation Method/Working On"
-            }, {
-                "data": "Probability Group"
-            }, {
-                "data": "Risk Group"
-            }, {
-                "data": "Damage Rate"
-            }, {
-                "data": "Risk Score"
-            }, {
-                "data": "Planned"
-            }, {
-                "data": "Overdue"
-            }, {
-                "data": "Cancelled"
-            }, {
-                "data": "Completed"
+        {
+            "data": "Work Done For (Adjusted)"
+        }, {
+            "data": "Excavation Method/Working On"
+        }, {
+            "data": "Probability Group"
+        }, {
+            "data": "Risk Group"
+        }, {
+            "data": "Damage Rate"
+        }, {
+            "data": "Risk Score"
+        }, {
+            "data": "Planned"
+        }, {
+            "data": "Overdue"
+        }, {
+            "data": "Cancelled"
+        }, {
+            "data": "Completed"
+        }, {
+            "data": "Human Strata"
+        }, {
+            "data": "Recommended Actions",
+            "render": function(data, type, full, meta) {
+                return '<button>Confirm!</button>' + data;
             }
-        ];
+        }
+    ];
 
     var table = $('#example').DataTable({
 
@@ -60,10 +69,18 @@ $(document).ready(function() {
 
         "ajax": "newtable.json",
         "columns": variables,
+        "columnDefs": [{
+            "targets": -1,
+            "data": null,
+            "defaultContent": "<button>Click!</button>"
+        }],
 
         "order": [
-            [1, "asc"]
+            [0, "asc"]
         ],
+
+        "searching": true,
+
 
         "paging": false,
         "info": false,
@@ -76,35 +93,65 @@ $(document).ready(function() {
             }
         },
 
+        
+/*
         "fnInitComplete": function(oSettings, json) {
             var table = $('#example').DataTable();
             $("#example thead th").each(function(i) {
                     var select = $('<select><option value="">Filter on:</option></select>')
                         .appendTo(this)
                         .on('change', function() {
-                            table.column(i)
-                                .search($(this).val())
-                                .draw();
-                        });
+                       var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        table.column(i)
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                table.column(i).unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
 
-                    table.column(i).data().unique().sort().each(function(d, j) {
-                        select.append('<option value="' + d + '">' + d + '</option>')
-                    });
+ 
 
                     if (select.children('option').length > 10) {
-                        select.hide();
+                        //select.hide();
                     }
 
                 }
 
             );
 
-            for (var i = 5; i < variables.length; i ++)
-            {
+
+
+
+        }  */
+               initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty())
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+
+                        for (var i = 5; i < variables.length - 2; i++) {
                 table.column(i).visible(false);
             }
-
-        }
+        } 
 
 
 
@@ -143,7 +190,43 @@ $(document).ready(function() {
         //alert( table.rows('.success').data().length +' row(s) selected' );
         if (table.rows('.success').data().length > 0)
             $('#myModal').modal('show');
+        else
+            alert("please select ticket(s) first!");
     });
+    $('#buttonOutcome').click(function() {
+        //alert( table.rows('.success').data().length +' row(s) selected' );
+        if (table.rows('.success').data().length > 0)
+            $('#myOutcomeModal').modal('show');
+        else
+            alert("please select ticket(s) first!");
+    });
+
+    $('#buttonAll').click(function() {
+        //alert( table.rows('.success').data().length +' row(s) selected' );
+        var table = $('#example').DataTable();
+        $("#example tbody tr").each(function(i) {
+                $(this).addClass('success');
+
+
+            }
+
+        );
+
+    });
+
+    $('#buttonNone').click(function() {
+        //alert( table.rows('.success').data().length +' row(s) selected' );
+        var table = $('#example').DataTable();
+        $("#example tbody tr").each(function(i) {
+                $(this).removeClass('success');
+
+
+            }
+
+        );
+
+    });
+
 
 
 
